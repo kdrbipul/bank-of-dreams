60c27f8eb5ec30d4932c1f937d9b1351f9db1430
kdrbipul <kdrbipul@gmail.com>
2023-07-21 18:31:55 +0600

    last commit

--- js/calculation.js
+++ js/calculation.js
@@ -3,4 +3,4 @@ const withdrawBtn=document.getElementById('withdraw-btn');
 // console.log(depositBtn, withdrawBtn);
 const depositField=document.getElementById('deposit-field');
 const withdrawField=document.getElementById('withdraw-field');
-console.log(depositField,withdrawField);
+// console.log(depositField,withdrawField); 
